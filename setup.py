07e042bf0b3048358a12a5942d5b15d8c029a6a0
Rajveer Kumar <rajveerkumar803101@gmail.com>
2026-05-15 15:39:29 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Green vision",
+    version="0.0.1",
+    author="pw_skills",
+    author_email="info@pw.live",
+    packages=find_packages(),
+    install_requires=[]
+)
